e21cd1a597692a0658c860c0315207eb7e7f3d96
Lawrence Babb <lbabb@broadinstitute.org>
2026-07-08 10:06:21 +0100

    Rename object_condition to object_condition_id(s) in proposition schemas

--- src/scripts/parquet-schemas/proposition.sql
+++ src/scripts/parquet-schemas/proposition.sql
@@ -4,7 +4,7 @@ SELECT
   JSON_VALUE(value, '$.type') AS type,
   JSON_VALUE(value, '$.predicate') AS predicate,
   REGEXP_REPLACE(JSON_VALUE(value, '$.subjectVariant'), r'^#/[^/]+/', '') AS subject_variant_id,
-  REGEXP_REPLACE(JSON_VALUE(value, '$.objectCondition'), r'^#/[^/]+/', '') AS object_condition,
+  REGEXP_REPLACE(JSON_VALUE(value, '$.objectCondition'), r'^#/[^/]+/', '') AS object_condition_id,
   JSON_VALUE(value, '$.geneContextQualifier.name') AS gene_context_name,
   TO_JSON_STRING(value) AS data
 FROM {DATASET}.gks_dict_proposition
