3dc012c9b4aa40e55e03307f330e51480711985b
Lawrence Babb <lbabb@broadinstitute.org>
2026-08-13 23:09:43 -0400

    fix(parquet): SCV gene_context_name reads qualifiers[] for custom propositions (was null)

--- src/scripts/parquet-schemas/proposition.sql
+++ src/scripts/parquet-schemas/proposition.sql
@@ -8,6 +8,9 @@ SELECT
   JSON_VALUE(value, '$.predicate') AS predicate,
   REGEXP_REPLACE(COALESCE(JSON_VALUE(value, '$.subjectVariant'), JSON_VALUE(value, '$.subject')), r'^#/[^/]+/', '') AS subject_variant_id,
   REGEXP_REPLACE(COALESCE(JSON_VALUE(value, '$.objectCondition'), JSON_VALUE(value, '$.object'), JSON_VALUE(value, '$.objectTumorType')), r'^#/[^/]+/', '') AS object_condition_id,
-  JSON_VALUE(value, '$.geneContextQualifier.name') AS gene_context_name,
+  COALESCE(
+    JSON_VALUE(value, '$.geneContextQualifier.name'),
+    (SELECT JSON_VALUE(q, '$.value.name') FROM UNNEST(JSON_QUERY_ARRAY(value, '$.qualifiers')) q WHERE JSON_VALUE(q, '$.name') = 'geneContext')
+  ) AS gene_context_name,
   TO_JSON_STRING(value) AS data
 FROM {DATASET}.gks_dict_proposition
